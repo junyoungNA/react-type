8c2f0244a03b6c246547318b34855450ff7f5843
junyoungNA <nanasooo@naver.com>
2023-06-11 18:41:49 +0900

    무한 캐러셀 구현중

--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -1,56 +1,83 @@
-import React, { useCallback } from 'react'
-import styled from 'styled-components';
+import React, { useCallback } from "react";
+import styled from "styled-components";
 import SvgIcon from "@mui/material/SvgIcon";
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import { VisualText } from '../assets/styled/common';
-import VisualList from '../assets/Data/data';
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { VisualText } from "../assets/styled/common";
 
 const ProgressBox = styled.div`
-    display: flex;
-    align-items: center;
-    position: absolute;
-    bottom: 30px;
-    left: 450px;
-    
-`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 30px;
+  left: 450px;
+`;
 
 const InputProgress = styled.input.attrs(props => ({
-    type: "range",
-    size: props.size || "100px",
-}))
-` 
-    margin:10px;
-    width: 200px;
-    height: 5px;
-    font-weight: 10px;
+  type: "range",
+  size: props.size || "100px",
+}))`
+  margin: 10px;
+  width: 200px;
+  height: 5px;
+  font-weight: 10px;
 `;
 
 type Props = {
-    setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
-    currentSlide: number
-    slideSize:number
-}
+  setCurrentSlide: React.Dispatch<React.SetStateAction<number>>;
+  currentSlide: number;
+  slideSize: number;
+  양끝에_추가될_데이터수: number;
+  resetCurrentSlide: () => void;
+};
 
-const Progressbar: React.ElementType<Props> = ({setCurrentSlide, currentSlide, slideSize}) => {
-    
-  const prevButtonClick = useCallback((currentSlide: number) => () => {
-    setCurrentSlide(currentSlide => (currentSlide - 1 + slideSize) % slideSize);
-  },[]);
+const Progressbar: React.ElementType<Props> = ({
+  setCurrentSlide,
+  currentSlide,
+  slideSize,
+  양끝에_추가될_데이터수,
+  resetCurrentSlide,
+}) => {
+  const prevButtonClick = useCallback(
+    (currentSlide: number) => () => {
+      setCurrentSlide(
+        currentSlide => (currentSlide - 1 + slideSize) % slideSize
+      );
+    },
+    []
+  );
 
-  const nextButtonClick =  useCallback((currentSlide: number) => () => {
+  const nextButtonClick = useCallback(
+    (currentSlide: number) => () => {
       setCurrentSlide(currentSlide => (currentSlide + 1) % slideSize);
-  }, []);
+      resetCurrentSlide();
+    },
+    [currentSlide]
+  );
 
-    return (
-        <ProgressBox>
-            <SvgIcon component={ArrowLeftIcon} inheritViewBox  style={{fontSize:'35px', cursor:'pointer'}} onClick={prevButtonClick(currentSlide)}/>
-            <VisualText color='black' fontSize='18px'>{currentSlide + 1}</VisualText>
-            <InputProgress />
-            <VisualText color='black' fontSize='18px'>{slideSize}</VisualText>
-            <SvgIcon component={ArrowRightIcon} inheritViewBox style={{fontSize:'35px', cursor:'pointer'}} onClick={nextButtonClick(currentSlide)}/>
-        </ProgressBox>
-    )
-}
+  return (
+    <ProgressBox>
+      <SvgIcon
+        component={ArrowLeftIcon}
+        inheritViewBox
+        style={{ fontSize: "35px", cursor: "pointer" }}
+        onClick={prevButtonClick(currentSlide)}
+      />
+      <VisualText color="black" fontSize="18px">
+        {currentSlide}
+      </VisualText>
+      <InputProgress />
+      <VisualText color="black" fontSize="18px">
+        {slideSize - 양끝에_추가될_데이터수 * 2}
+      </VisualText>
+      <SvgIcon
+        component={ArrowRightIcon}
+        inheritViewBox
+        style={{ fontSize: "35px", cursor: "pointer" }}
+        onClick={nextButtonClick(currentSlide)}
+      />
+    </ProgressBox>
+  );
+};
 
-export  default Progressbar;
+export default Progressbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,60 @@
-import React, {useState, useEffect, useRef} from 'react'
-import styled from 'styled-components';
-import VisualList from '../assets/Data/data';
-import Progressbar from '../components/Progressbar';
-import {VisualInner, VisualText } from '../assets/styled/common'
+import React, { useState, useCallback } from "react";
+import styled from "styled-components";
+import VisualList from "../assets/Data/data";
+import Progressbar from "../components/Progressbar";
+import { VisualInner, VisualText } from "../assets/styled/common";
 
 // background
 // #a6a8ac
-// #131212 
+// #131212
 
 //font
 // e0e0e0
 // 383838
 
 const VisualContainer = styled.div`
-    position: relative;
-    overflow: hidden;
-    background-color: whitesmoke;
-    height: 450px;
-`  
+  position: relative;
+  overflow: hidden;
+  background-color: whitesmoke;
+  height: 450px;
+`;
 
 const SlideBox = styled.div`
-    overflow: hidden;
-    height: inherit;
-    margin: auto;
-    width: 1000px;
-`
+  overflow: hidden;
+  height: inherit;
+  margin: auto;
+  width: 1000px;
+`;
 
-const SlideTrack = styled.div<{offset: number}>`
-    display: flex;
-    height: inherit;
-    transition: transform 0.3s ease-in-out;
-    transform: translateX(${props => props.offset}%);
-`
+const SlideTrack = styled.div<{ offset: number }>`
+  display: flex;
+  height: inherit;
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(${props => props.offset}%);
+`;
 
 const MainContainer = styled.section`
-    height: 700px;
-`
+  height: 700px;
+`;
 
 const HomeSection = styled.section`
-    margin: auto;
-    background-color:#131212;
-`
-
+  margin: auto;
+  background-color: #131212;
+`;
 
 const VisualImage = styled.img`
-    position: absolute;
-    width: 450px;
-    top: 0;
-    right: -31px;
-    height:450px;
-    border-radius: 20px;
-`
+  position: absolute;
+  width: 450px;
+  top: 0;
+  right: -31px;
+  height: 450px;
+  border-radius: 20px;
+`;
 
 const Home: React.FC = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);  
-    const 양끝에_추가될_데이터수 = 1;
+  const 양끝에_추가될_데이터수 = 1;
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const [currOffSet, setCurrOffset] = useState(-currentSlide * 100);
 
   const setSlides = () => {
     let addedFront = [];
@@ -62,66 +62,66 @@ const Home: React.FC = () => {
     let index = 0;
     while (index < 양끝에_추가될_데이터수) {
       addedLast.push(VisualList[index % VisualList.length]);
-      addedFront.unshift(VisualList[VisualList.length - 1 - (index % VisualList.length)]);
+      addedFront.unshift(
+        VisualList[VisualList.length - 1 - (index % VisualList.length)]
+      );
       index++;
     }
     return [...addedFront, ...VisualList, ...addedLast];
-  }
+  };
 
-  const SLIDE_SIZE = setSlides().length; // 슬라이드 전체 사이즈
+  const SLIDE_SIZE = setSlides().length; //보여질 슬라이드 전체 사이즈
   const transitionTime = 500;
-const transitionStyle = `transform ${transitionTime}ms ease 0s`;
+  const transitionStyle = `transform ${transitionTime}ms ease 0s`;
 
-// function replaceSlide(index) {
-//   setTimeout(() => {
-//     setTransition('');
-//     setCurrentIndex(index);
-//   }, transitionTime)
-// }
+  const resetCurrentSlide = () => {
+    if (currentSlide >= SLIDE_SIZE - 양끝에_추가될_데이터수 * 2) {
+      console.log("움직여");
+      setTimeout(() => {
+        setCurrentSlide(1);
+      }, 0);
+    }
+  };
 
-// function handleSwipe(direction) {
-//   let index = currentIndex + direction;
-//   setCurrentIndex(index);
-//   if (index < 양끝에_추가될_데이터수) {
-//     index += itemSize;
-//     replaceSlide(index)
-//   }
-//   else if (index >= itemSize + 양끝에_추가될_데이터수) {
-//     index -= - itemSize;
-//     replaceSlide(index)
-//   }
-//   setTransition(transitionStyle);
-// }
+  return (
+    <HomeSection>
+      <VisualContainer style={{ background: "#a3a3a3 " }}>
+        <SlideBox>
+          <SlideTrack offset={currOffSet}>
+            {setSlides()?.map((visual, index) => {
+              return (
+                <VisualInner
+                  width={visual.width}
+                  height={visual.height}
+                  key={index}
+                >
+                  {visual.text.map((text, index) => (
+                    <VisualText
+                      color={visual.font[index].color}
+                      fontSize={visual.font[index].fontSize}
+                      fontWeight={visual.font[index].fontWeight}
+                      key={index}
+                    >
+                      {text}
+                    </VisualText>
+                  ))}
+                  <VisualImage src={visual.img} />
+                </VisualInner>
+              );
+            })}
+          </SlideTrack>
+          <Progressbar
+            setCurrentSlide={setCurrentSlide}
+            currentSlide={currentSlide}
+            slideSize={SLIDE_SIZE}
+            양끝에_추가될_데이터수={1}
+            resetCurrentSlide={resetCurrentSlide}
+          ></Progressbar>
+        </SlideBox>
+      </VisualContainer>
+      <MainContainer></MainContainer>
+    </HomeSection>
+  );
+};
 
-
-
-    return (
-        <HomeSection>
-            <VisualContainer style={{background:'#a3a3a3 '}}>
-                <SlideBox >
-                    <SlideTrack offset={-currentSlide * 100}>
-                    {setSlides()?.map((visual, index) => {
-                      console.log(visual);
-                      return(
-                        <VisualInner width={visual.width} height={visual.height}>
-                          {visual.text.map((text, index) =>  
-                          <VisualText color={visual.font[index].color} fontSize={visual.font[index].fontSize} fontWeight={visual.font[index].fontWeight}>
-                              {text} 
-                          </VisualText> )}
-                          <VisualImage src={visual.img}/>
-                        </VisualInner >
-                      )
-                    }
-                    )}
-                    </SlideTrack>
-                    <Progressbar setCurrentSlide={setCurrentSlide} currentSlide={currentSlide} slideSize={SLIDE_SIZE}></Progressbar>
-                </SlideBox>
-            </VisualContainer>
-            <MainContainer>
-            </MainContainer>
-        </HomeSection>
-    )
-}
-
-
-export default Home
+export default Home;
